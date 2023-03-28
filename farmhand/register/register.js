2f71ac389c8be04f01d410057a3fd385feaa98cd
bagortechnology <hello.bagortech@gmail.com>
2023-03-28 08:29:47 +0800

    new file:   farmhand/register/register.js

---
+++ farmhand/register/register.js
@@ -0,0 +1,27 @@
+const db = require("../dbconfig");
+
+router.post("/submit-registration", async (req, res) => {
+  try {
+    const { email, password, role } = req.body;
+
+    // Check if user already exists
+    const userExists = await db.query("SELECT * FROM users WHERE email = $1", [
+      email
+    ]);
+
+    if (userExists.rowCount > 0) {
+      return res.status(400).send("User already exists.");
+    }
+
+    // Insert user into the database
+    await db.query(
+      "INSERT INTO users (email, password, role) VALUES ($1, $2, $3)",
+      [email, password, role]
+    );
+
+    return res.status(200).send("Registration successful.");
+  } catch (err) {
+    console.log(err);
+    return res.status(500).send("Internal server error.");
+  }
+});
