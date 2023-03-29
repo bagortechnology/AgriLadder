ca7819c51e08df1281e19ff92029dbdce44b5dc8
bagortechnology <hello.bagortech@gmail.com>
2023-03-29 17:24:17 +0800

    modified:   farmhand/register/index.html 	modified:   src/index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,8 @@
 
-// Import the functions you need from the SDKs you need
+
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.18.0/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.18.0/firebase-analytics.js";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyA-QhUuEMFrtMnXLsKx2-QwoZBnnZsxFK4",
   authDomain: "agriladder-ba481.firebaseapp.com",
@@ -17,6 +13,6 @@ const firebaseConfig = {
   measurementId: "G-18GXH26E02"
 };
 
-// Initialize Firebase
+
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
