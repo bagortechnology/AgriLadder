e07f4f56277effd71e64964bb7be18447b4e81da
bagortechnology <hello.bagortech@gmail.com>
2023-03-29 23:26:46 +0800

    modified:   farmhand/register/fh-reg.html 	modified:   farmhand/register/register.js

--- farmhand/register/register.js
+++ farmhand/register/register.js
@@ -72,10 +72,10 @@ $('.reveal-confirm').click(function() {
         if (form.checkValidity() === false || passwordStrengthScore < 2) {
             event.preventDefault();
             event.stopPropagation();
-
+    
             // add 'was-validated' class to show validation feedback
             form.classList.add("was-validated");
-
+    
             // show error message for password strength
             if (passwordStrengthScore < 2) {
                 $('#password-strength').removeClass().addClass('text-danger small').text('Password must be at least medium strength.');
@@ -84,26 +84,43 @@ $('.reveal-confirm').click(function() {
             // clear any error messages
             $('#password-strength').removeClass().addClass('text-muted small').text('');
             form.classList.add("was-validated");
-
+    
             // check if password and confirm password match
             const password = document.getElementById("pwdRegFh");
             const confirmPassword = document.getElementById("confirmPwdRegFh");
-
+    
             if (password.value !== confirmPassword.value) {
                 // prevent form submission if passwords don't match
                 event.preventDefault();
                 event.stopPropagation();
-
+    
                 // add 'was-validated' class to show validation feedback
                 form.classList.add("was-validated");
-
+    
                 // show error message for confirm password
                 confirmPassword.setCustomValidity("Passwords must match");
             } else {
                 confirmPassword.setCustomValidity("");
+                // create user with email and password
+                const email = document.getElementById("emailRegFh").value;
+                const password = document.getElementById("pwdRegFh").value;
+                createUserWithEmailAndPassword(auth, email, password)
+                .then((userCredential) => {
+                  // Signed in 
+                  const user = userCredential.user;
+                  // ...
+                  alert('You are successfully registered!')
+                })
+                .catch((error) => {
+                  const errorCode = error.code;
+                  const errorMessage = error.message;
+                  // ..
+                  alert(errorMessage);
+                });
             }
         }
     });
+    
 
     // add event listener to remove custom validity on input change
     const confirmPassword = document.getElementById("confirmPwdRegFh");
