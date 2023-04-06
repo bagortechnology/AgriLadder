3e54d890cd60bd57d239a73992bd416c60dc8985
bagortechnology <hello.bagortech@gmail.com>
2023-04-06 11:11:35 +0800

    new file:   assets/js/typed.js

---
+++ assets/js/typed.js
@@ -0,0 +1,8 @@
+const typed = new Typed("#hero-job", {
+    strings: ["love.", "enjoy.", "cherish.", "are passionate about."],
+    typeSpeed: 100,
+    backSpeed: 50,
+    loop: true,
+    loopCount: null,
+    smartBackspace: true,
+  });
