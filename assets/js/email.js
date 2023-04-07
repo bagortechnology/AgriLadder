29d34b1c5c9adc2d737dc7e1c157d49d7412a2a7
bagortechnology <hello.bagortech@gmail.com>
2023-04-07 22:10:23 +0800

    new file:   assets/js/email.js 	modified:   contact.html

---
+++ assets/js/email.js
@@ -0,0 +1,19 @@
+function sendEmail() {
+    let FName = document.getElementById("inputFName").value;
+    let LName = document.getElementById("inputLName").value;
+    let subject = document.getElementById("inputSubject").value;
+    let email = document.getElementById("inputEmail").value;
+    let message = document.getElementById("inputMessage").value;
+
+    let body = "Name: " + FName + "&nbsp" + LName +"</br> Message: " + message;
+
+    Email.send({
+        SecureToken : "b1764bdb-aab4-4b6c-9097-dced334622f5",
+        To : 'agriladder@gmail.com',
+        From : email,
+        Subject : subject,
+        Body : body
+    }).then(
+      message => alert(message)
+    );
+}
