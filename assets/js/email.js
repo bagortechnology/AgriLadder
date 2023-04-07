abe43f6edef3e192f665ba7e59e5eeda9503a162
bagortechnology <hello.bagortech@gmail.com>
2023-04-07 23:42:40 +0800

    modified:   assets/js/email.js

--- assets/js/email.js
+++ assets/js/email.js
@@ -1,12 +1,28 @@
 function sendEmail() {
-    let FName = document.getElementById("inputFName").value;
-    let LName = document.getElementById("inputLName").value;
-    let subject = document.getElementById("inputSubject").value;
-    let email = document.getElementById("inputEmail").value;
-    let message = document.getElementById("inputMessage").value;
+    // Get input values
+    let FName = document.getElementById("inputFName").value.trim();
+    let LName = document.getElementById("inputLName").value.trim();
+    let subject = document.getElementById("inputSubject").value.trim();
+    let email = document.getElementById("inputEmail").value.trim();
+    let message = document.getElementById("inputMessage").value.trim();
 
-    let body = "Name: " + FName + " " + LName +"<br/> Message: " + message;
+    // Validate input values
+    if (FName === "" || LName === "" || subject === "" || email === "" || message === "") {
+        alert("Please fill in all fields.");
+        return;
+    }
 
+    // Validate email format
+    let emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+        alert("Please enter a valid email address.");
+        return;
+    }
+
+    // Compose email body
+    let body = "Name: " + FName + " " + LName + "<br/> Message: " + message;
+
+    // Send email to recipient
     Email.send({
         SecureToken : "324dbbf6-80a6-48b0-bbc0-8b9e54ba427f",
         To : email,
@@ -14,6 +30,31 @@ function sendEmail() {
         Subject : subject,
         Body : body
     }).then(
-      message => alert(message)
+      recipientMessage => {
+        // Send email to owner
+        Email.send({
+          SecureToken : "324dbbf6-80a6-48b0-bbc0-8b9e54ba427f",
+          To : "agriladder@gmail.com",
+          From : "info@agriladder.com",
+          Subject : "New message from " + FName + " " + LName,
+          Body : "Sender Email: " + email + "<br/>" + recipientMessage 
+        }).then(
+          ownerMessage => {
+            // Clear input fields and display success alert
+            document.getElementById("inputFName").value = "";
+            document.getElementById("inputLName").value = "";
+            document.getElementById("inputSubject").value = "";
+            document.getElementById("inputEmail").value = "";
+            document.getElementById("inputMessage").value = "";
+            alert("Email sent to successfully!");
+          },
+          ownerError => {
+            alert("Error sending email!");
+          }
+        );
+      },
+      recipientError => {
+        alert("Error sending email!");
+      }
     );
 }
