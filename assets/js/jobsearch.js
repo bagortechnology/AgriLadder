e05526ab210a3fdb26fc53e8375f388e97ccb32e
bagortechnology <hello.bagortech@gmail.com>
2023-04-08 13:37:17 +0800

    modified:   assets/js/jobsearch.js 	modified:   jobs.html

--- assets/js/jobsearch.js
+++ assets/js/jobsearch.js
@@ -1,17 +1,15 @@
 function searchJobs() {
-  // Get all job postings
-  const jobPostings = document.querySelectorAll('#job-postings .job-item');
-  // Save a reference to the original job postings
-  const originalPostings = Array.from(jobPostings);
-
-  // Get search input values
-  const jobSearchValue = document.querySelector('#job-search').value.toLowerCase();
-  const locationSearchValue = document.querySelector('#location-search').value.toLowerCase();
-
-  // Filter job postings based on search input values or reset to original postings
-  let filteredPostings;
-  if (jobSearchValue !== '' || locationSearchValue !== '') {
-    filteredPostings = originalPostings.filter(posting => {
+    // Get all job postings
+    const jobPostings = document.querySelectorAll('#job-postings .job-item');
+    // Save a reference to the original job postings
+    const originalPostings = Array.from(jobPostings);
+  
+    // Get search input values
+    const jobSearchValue = document.querySelector('#job-search').value.toLowerCase();
+    const locationSearchValue = document.querySelector('#location-search').value.toLowerCase();
+  
+    // Filter job postings based on search input values
+    const filteredPostings = originalPostings.filter(posting => {
       const jobTitle = posting.querySelector('.lead').textContent.toLowerCase();
       const location = posting.querySelector('.fa-map-marker-alt').nextSibling.textContent.toLowerCase();
       if (jobSearchValue !== '' && locationSearchValue !== '') {
@@ -22,24 +20,26 @@ function searchJobs() {
         return location.includes(locationSearchValue);
       }
     });
-  } else {
-    filteredPostings = originalPostings;
+  
+    // Create new HTML element to display filtered job postings or alert message
+    const filteredPostingsHTML = document.createElement('div');
+    filteredPostingsHTML.setAttribute('id', 'job-postings');
+    if (filteredPostings.length > 0) {
+      filteredPostings.forEach(posting => {
+        filteredPostingsHTML.appendChild(posting.cloneNode(true));
+      });
+    } else {
+      const alertMessage = document.createElement('p');
+      alertMessage.textContent = 'No job postings match your search criteria. Please contact us for assistance.';
+      filteredPostingsHTML.appendChild(alertMessage);
+    }
+  
+    // Replace current job postings with filtered job postings or original postings
+    const currentPostings = document.querySelector('#job-search-results');
+    if (jobSearchValue === '' && locationSearchValue === '') {
+      currentPostings.parentNode.replaceChild(document.createElement('div'), currentPostings);
+    } else {
+      currentPostings.parentNode.replaceChild(filteredPostingsHTML, currentPostings);
+    }
   }
-
-  // Create new HTML element to display filtered job postings or alert message
-  const filteredPostingsHTML = document.createElement('div');
-  filteredPostingsHTML.setAttribute('id', 'job-postings');
-  if (filteredPostings.length > 0) {
-    filteredPostings.forEach(posting => {
-      filteredPostingsHTML.appendChild(posting.cloneNode(true));
-    });
-  } else {
-    const alertMessage = document.createElement('p');
-    alertMessage.textContent = 'No job postings match your search criteria. Please contact us for assistance.';
-    filteredPostingsHTML.appendChild(alertMessage);
-  }
-
-  // Replace current job postings with filtered job postings or original postings
-  const currentPostings = document.querySelector('#job-search-results');
-  currentPostings.parentNode.replaceChild(filteredPostingsHTML, currentPostings);
-}
+  
